41d593993049af15f6e7358160b1aa2bdfa5cb45
allImran <badhan19@gmail.com>
2020-08-25 08:34:48 +0600

    blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -6,30 +6,53 @@
 		/>
 		<v-container>
 			<v-row>
-				<v-col cols="12" md="8">
+				<v-col cols="12" md="9">
 					<v-row>
-						<v-col cols="12" md="6">
-							<BlogListHover />
-						</v-col>
+						<template v-for="(item, index) in items">
+							<v-col :key="index" cols="12" md="6">
+								<BlogListHover
+									
+									:item="item"
+								/>
+							</v-col>
+						</template>
 					</v-row>
 				</v-col>
-				<v-col cols="12" md="4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nesciunt atque ipsa similique pariatur aliquam libero necessitatibus ut sit voluptatem. Officiis nam excepturi eaque labore sequi, amet veritatis reprehenderit nulla?</v-col>
+				<v-col cols="12" md="3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nesciunt atque ipsa similique pariatur aliquam libero necessitatibus ut sit voluptatem. Officiis nam excepturi eaque labore sequi, amet veritatis reprehenderit nulla?</v-col>
 			</v-row>
 		</v-container>
+		<v-overlay color="#fff" :value="loading">
+			<img src="/loader.gif" alt="">
+		</v-overlay>
+		<Observer @intersect="intersecting" />
 	</div>
 </template>
 <script>
 	import Breadcrumb from '@/components/Breadcrumb'
-	import BlogListHover from '@/components/blog/BlogListHover' 
+	import BlogListHover from '@/components/blog/BlogListHover'
+	import Observer from '@/components/Observer'	
 	export default {
 		name: 'blog',
-		components: {Breadcrumb, BlogListHover},
+		components: {Breadcrumb, BlogListHover, Observer},
 		data: () => ({
 			pageTitle: 'blog',
 			breadCrumbData: [
 				{title: 'Home', url: '/'},
 				{title: 'Blog', url: '#'}
 			],
+			items: [],
+			loading: false,
+			 page: 1
 		}),
+		methods: {
+			async intersecting(){
+				this.loading = true
+				const res = await fetch(`http://jsonplaceholder.typicode.com/photos?_page=${this.page}`);
+				let items = await res.json();
+				this.items = [...this.items, ...items];
+				this.page++
+				this.loading = false
+			}
+		}
 	}
 </script>
