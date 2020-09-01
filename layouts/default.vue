6150555da3f403807204d5c1e33d96555382538e
neutral-Stage <anirbanroy691@gmail.com>
2020-09-01 13:32:51 +0600

    default change

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,19 +12,18 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      fixed
-      app
-      elevation="0"
-      color="#fff"
-      height="100"
-      class="pa-xl-6 pa-lg-5 pa-md-4 pa-sm-3 pa-2"
-    >
-      <v-app-bar-nav-icon class="hidden-sm-and-up" @click.stop="drawer = !drawer" />
-
-      <v-img src="/logo@2x.png" max-width="40"></v-img>
-
-      <v-toolbar-title class="pl-3 pr-12" v-text="title" />
+    <v-app-bar fixed app elevation="0" color="#fff" class="pl-lg-6 pl-sm-5 pl-0" height="80">
+      <v-app-bar-nav-icon
+        class="hidden-sm-and-up mr-2"
+        color="primary"
+        @click.stop="drawer = !drawer"
+      />
+      <nuxt-link to="/">
+        <v-img src="/logo@2x.png" max-width="40"></v-img>
+      </nuxt-link>
+      <nuxt-link to="/" tag="v-toolbar-title">
+        <v-toolbar-title class="pl-3 pr-12 primary--text" v-text="title" />
+      </nuxt-link>
 
       <v-btn
         @click="goToUrl(menu.url)"
